0441afb60a6ccee870968f454386b18f7e295e07
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-10-17 11:39:50 -0700

    Account for no account yet

--- app/people/[recordID]/profile-page.tsx
+++ app/people/[recordID]/profile-page.tsx
@@ -4,18 +4,21 @@ import Image from "next/image";
 
 export function ProfilePage(props: {
   guest: GuestProfile;
-  guestAccount: User;
+  guestAccount?: User;
   isUsersProfile: boolean;
 }) {
   const { guest, guestAccount, isUsersProfile } = props;
   return (
     <div>
-      <Image
-        src={guestAccount.imageUrl}
-        alt={guest.Name}
-        height="100"
-        width="100"
-      />
+      {guestAccount && (
+        <Image
+          src={guestAccount.imageUrl}
+          alt={guest.Name}
+          height="100"
+          width="100"
+          className="rounded-full"
+        />
+      )}
       <h1>{guest.Name}</h1>
       <p>{guest.Bio}</p>
       {isUsersProfile && <p>This is your profile</p>}
