fad38fab2e1fde07cbd520e5aee268a40941a695
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-10-17 11:08:59 -0700

    Add check for whether this is user's profile

--- app/people/[recordID]/page.tsx
+++ app/people/[recordID]/page.tsx
@@ -1,8 +1,15 @@
 import { getGuestByID } from "@/db/guests";
 import { ProfilePage } from "./profile-page";
+import { auth } from "@clerk/nextjs/server";
+
+type Metadata = undefined | { record_id: string };
 
 export default async function Page(props: { params: { recordID: string } }) {
   const { recordID } = props.params;
   const guest = await getGuestByID(recordID);
-  return <ProfilePage guest={guest} />;
+  const { sessionClaims } = auth();
+  const metadata = sessionClaims?.metadata as Metadata;
+  const userRecordID = metadata?.record_id;
+  const isUsersProfile = userRecordID === recordID;
+  return <ProfilePage guest={guest} isUsersProfile={isUsersProfile} />;
 }
--- app/people/[recordID]/profile-page.tsx
+++ app/people/[recordID]/profile-page.tsx
@@ -1,10 +1,15 @@
 import { GuestProfile } from "@/db/guests";
 
-export function ProfilePage(props: { guest: GuestProfile }) {
+export function ProfilePage(props: {
+  guest: GuestProfile;
+  isUsersProfile: boolean;
+}) {
+  const { guest, isUsersProfile } = props;
   return (
     <div>
-      <h1>{props.guest.Name}</h1>
-      <p>{props.guest.Bio}</p>
+      <h1>{guest.Name}</h1>
+      <p>{guest.Bio}</p>
+      {isUsersProfile && <p>This is your profile</p>}
     </div>
   );
 }
