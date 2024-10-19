3950c9ea9989eb48006f26b0e4532e6e75b1fa48
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-10-19 13:06:55 -0700

    Move fetches for people page into Promise.all

--- app/people/page.tsx
+++ app/people/page.tsx
@@ -3,7 +3,6 @@ import { getGuestProfiles } from "@/db/guests";
 import { PeopleDisplay } from "./people-display";
 
 export default async function PeoplePage() {
-  const users = await getUsers();
-  const guests = await getGuestProfiles();
+  const [users, guests] = await Promise.all([getUsers(), getGuestProfiles()]);
   return <PeopleDisplay users={users} guests={guests} />;
 }
