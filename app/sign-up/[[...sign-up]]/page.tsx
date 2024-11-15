b689ca00d0fa6dc62ed983aa4b173c0fcb57c0c5
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-11-15 11:32:55 -0800

    Remove sign up page and add info about invitations instead

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,15 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
-  return <SignUp />;
+  return (
+    <div className="mx-auto">
+      In order to sign up, you&apos;ll need to follow your individual invitation
+      link, which you should have recieved in an email with the subject line{" "}
+      <span className="font-bold">
+        &quot;Invitation to join The Curve&quot;
+      </span>
+      . If you did not receive this invitation, but believe you should have,
+      email <span className="font-bold">rachel@thecurve.is</span>.
+    </div>
+  );
 }
